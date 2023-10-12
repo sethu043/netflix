c21cbba83be317f92ee4db504c27b42a02cafb0d
sethu043 <147687301+sethu043@users.noreply.github.com>
2023-10-12 11:55:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+     margin: 10,
+    nav: false,
+    autoplay: true,
+     autoplayTimeout: 2000,
+    
+     responsive: {
+     0: {
+             items: 1,
+    
+     },
+    600: {
+    items: 2,
+    
+     },
+    1000: {
+     items: 4,
+    
+    }
+    }
+})
